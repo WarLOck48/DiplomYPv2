c1139f7781c0177a76db937a220521a87e2bc3f3
Александр <153442485+WarLOck48@users.noreply.github.com>
2023-12-17 23:20:19 +0300

    Add files via upload

---
+++ Configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://593a8ac8-5df9-49e5-891b-8e6cfdfa5463.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_payload = {"firstName": "Alexander",
+                 "lastName": "Makarov",
+                 "address": "Kolomna, 178 apt.",
+                 "metroStation": 8,
+                 "phone": "+7 800 555 35 35",
+                 "rentTime": 5,
+                 "deliveryDate": "2020-11-20",
+                 "comment": "Hello",
+                 "color": ["BLACK"]}
