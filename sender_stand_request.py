90b5847abbb460662c6a95746dd754cf708e1c5a
Александр <153442485+WarLOck48@users.noreply.github.com>
2023-12-20 23:58:18 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,22 +1,20 @@
-import Configuration
+import configuration
 import requests
 import data
 
+def create_order(body):
+    """
+    Создание заказа
+    """
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
+                         json=body,
+                         headers=data.HEADERS)
 
-# Функция создания заказа
-def create_order(order_body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=order_body)
-
-
-# Функция по запоминанию трека
-def get_order_track():
-    track_response = create_order(data.order_body)
-    return track_response.json()["track"]
-
-
-# Функция получения заказа по треку
-
-def get_order_info_by_track():
-    track = get_order_track()
-    par = {"t": track}
-    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_INFO_BY_TRACK_PATH, params=par)
+def get_order_info(track):
+    """
+    Получение информации о заказе.
+    """
+    params = {'t': track}
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_INFO_PATH,
+                        params=params,
+                        headers=data.HEADERS)
